fb1785369b9623faeeba42e946ab37e1b88de29e
AnthonyDeMatos <Am1601091109112000+>
2022-07-13 11:50:50 -0300

    Nova Classe da Aula 49 (Exercícios)

---
+++ src/lista_exercicio_03/Exercicio_Estrutura_Repetitiva01.java
@@ -0,0 +1,22 @@
+package lista_exercicio_03;
+
+import java.util.Scanner;
+
+public class Exercicio_Estrutura_Repetitiva01 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = sc.nextInt();
+        }
+
+        System.out.println("Acesso Permitido");
+
+        sc.close();
+    }
+}
