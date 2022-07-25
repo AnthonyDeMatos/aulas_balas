d41e27a098d9391d154aa42d20efcc76a68d2818
AnthonyDeMatos <Am1601091109112000+>
2022-07-25 09:22:39 -0300

    Atualizado o nome da Classe da Aula 66

--- src/application/ProgramProduct.java
+++ src/application/ProgramProduct.java
@@ -5,7 +5,7 @@ import entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ProgramProduto {
+public class ProgramProduct {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
